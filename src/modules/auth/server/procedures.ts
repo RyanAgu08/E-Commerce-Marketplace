91b13c6fc8b5cc1cac5468485daba4df392da356
Ryan Aguilar <ryan.aguilar01@utrgv.edu>
2025-09-12 09:46:13 -0500

    09: auth states

--- src/modules/auth/server/procedures.ts
+++ src/modules/auth/server/procedures.ts
@@ -1,9 +1,9 @@
-import { headers as getHeaders, cookies as getCookies } from 'next/headers';
+import { headers as getHeaders } from 'next/headers';
 
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { TRPCError } from '@trpc/server';
-import { AUTH_COOKIE } from '../constants';
 import { loginSchema, registerSchema } from '../schemas';
+import { generateAuthCookie } from '../utils';
 
 export const authRouter = createTRPCRouter({
     session: baseProcedure.query(async ({ ctx }) => {
@@ -12,10 +12,6 @@ export const authRouter = createTRPCRouter({
         const session = await ctx.db.auth({ headers });
 
         return session;
-    }),
-    logout: baseProcedure.mutation(async () => {
-        const cookie = await getCookies();
-        cookie.delete(AUTH_COOKIE);
     }),
     register: baseProcedure
         .input(registerSchema)
@@ -62,15 +58,9 @@ export const authRouter = createTRPCRouter({
                     });
                 }
     
-                const cookies = await getCookies();
-                cookies.set({
-                    name: AUTH_COOKIE,
+                await generateAuthCookie({
+                    prefix: ctx.db.config.cookiePrefix,
                     value: data.token,
-                    httpOnly: true,
-                    path: "/",
-                    // TODO: Ensure cross-domain cookie sharing
-                    // sameSite: "none",
-                    // domain: ""
                 });
         }),
 
@@ -92,15 +82,9 @@ export const authRouter = createTRPCRouter({
                 });
             }
 
-            const cookies = await getCookies();
-            cookies.set({
-                name: AUTH_COOKIE,
+            await generateAuthCookie({
+                prefix: ctx.db.config.cookiePrefix,
                 value: data.token,
-                httpOnly: true,
-                path: "/",
-                // TODO: Ensure cross-domain cookie sharing
-                // sameSite: "none",
-                // domain: ""
             });
 
             return data;
--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -7,7 +7,7 @@ import { Poppins } from "next/font/google";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Input }  from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
@@ -33,14 +33,17 @@ export const SignInView = () => {
     const router = useRouter();
 
     const trpc = useTRPC();
+    const queryClient = useQueryClient();
+
     const login = useMutation(trpc.auth.login.mutationOptions({
         onError: (error) => {
             toast.error(error.message);
         },
-        onSuccess: () => {
+        onSuccess: async () => {
+            await queryClient.invalidateQueries(trpc.auth.session.queryFilter());
             router.push("/");
-        },
-    }));
+        }
+    }))
 
     const form = useForm<z.infer<typeof loginSchema >>({
         mode: "all",
